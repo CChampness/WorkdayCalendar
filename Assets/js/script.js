8a8e5be1715ce1f9c1f57454fe2e8a1b914669b0
CChampness <87551272+CChampness@users.noreply.github.com>
2021-09-27 12:02:17 -0400

    hard-coded the html for the hours

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,35 @@
-const numHours=10;
+const managedHours = [7,8,9,10,11,12,1,2,3,4,5,6];
+const numHours = managedHours.length;
 var dateTime = $('#currentDay');
 // console.log("dateTime:" + dateTime(text));
+
+function updateTimeBlocks(currentHour) {
+  console.log("updateTimeBlocks " + currentHour);
+  for(var pos=0; managedHours[pos] != currentHour; pos++);
+  for(var i=0; i<numHours; i++) {
+    var hour = managedHours[i];
+    console.log("hour:"+hour);
+    if (i < pos) {
+      // Change color for past hours to gray
+      console.log("less than hour:"+hour);
+      $("#" + hour).removeClass("present");
+      $("#" + hour).removeClass("future");
+      $("#" + hour).addClass("past");
+    } else if (i == pos) {
+      // Change color for currentHour to red
+      console.log("equal hour:"+hour);
+      $("#" + hour).removeClass("past");
+      $("#" + hour).removeClass("future");
+      $("#" + hour).addClass("present");
+    } else {
+      // Change color for future hours to green
+      console.log("more than hour:"+hour);
+      $("#" + hour).removeClass("past");
+      $("#" + hour).removeClass("present");
+      $("#" + hour).addClass("future");
+    }
+  }
+}
 
 function minuteCounter() {
   var currentTime;
@@ -13,21 +42,17 @@ function minuteCounter() {
   var lastHour = currentHour;
   console.log("lastHour:" + lastHour+", currentHour:" +currentHour);
  
-  function updateTimeBlocks() {
-    // Change color for new currentHour to red
-    // Change color for lastHour to gray
-    console.log("updateTimeBlocks");
-  }
-  
   // Use the `setInterval()` to go off every minute
   var timeInterval = setInterval(function() {
         
     currentTime = moment().format("MMMM Do, YYYY h:mm A");
+    console.log(currentTime);
+
     // Set the time display in the header
     dateTime.text(currentTime);
     if (lastHour != currentHour) {
       // The hour has rolled over, so handle the timeblock updates
-      updateTimeBlocks();
+      updateTimeBlocks(currentHour);
       lastHour = currentHour;
     }
     currentHour = moment().format("hh");
@@ -38,22 +63,39 @@ function minuteCounter() {
   function setupRows(numHours){
     var containerEl = $('.container');
     for (var i=0; i<numHours; i++){
-      var rowEl = containerEl.append("<div>ROW</div>");
-      console.log("row:" + i + " " +rowEl);
-      rowEl.addClass(["row", "time-block"]);
-      var hourEl = rowEl.append("<div>hours</div>");
-      hourEl.addClass(["col", "col-2", "hour"]);
-      var eventEl = rowEl.append("<div>event</div>");
-      hourEl.addClass(["col", "col-7", "description", "present"]);
-      var saveBtnEl = rowEl.append("<div>save</div>");
-      saveBtnEl.addClass(["col", "col-1", "saveBtn"]);
+      var t = $(document.createElement('div'))
+      var rowEl = containerEl.append("<div class='row time-block'>newrow</div>").children();
+      console.log(rowEl);
+      // rowEl.append(t);
+      let hourEl = rowEl.append("<div>hour</div>");
+      console.log(hourEl);
+      console.log(rowEl);
+      // let eventEl = rowEl.appendChild("<div>event</div>");
+      // let saveBtnEl = rowEl.appendChild("<div>save</div>");
 
-      // rowEl.text("AM/PM");
-      // console.log(rowEl.text());
-      // containerEl.append(rowEl);
+      // var rowEl = containerEl.append("<div class='row time-block'>newrow</div>");
+      // var hourEl = rowEl.append("<div class='col col-2 hour'></div>");
+      // var eventEl = rowEl.append("<div class='col col-7 description present'>event</div>");
+      // var saveBtnEl = rowEl.append("<div class='col col-1 savebtn'>save</div>");
+     
+      // var rowEl = containerEl.add("div");
+      // rowEl.addClass(["row", "time-block"]);
+      // var hourEl = rowEl.add("div");
+      // hourEl.addClass(["col", "col-2", "hour"]);
+      // var eventEl = rowEl.add("div");
+      // hourEl.addClass(["col", "col-7", "description", "present"]);
+      // var saveBtnEl = rowEl.add("div");
+      // saveBtnEl.addClass(["col", "col-1", "saveBtn"]);
       console.log("setupRows:"+i);
     }
   }
 
+  function saveEvent(hour) {
+    console.log("saveEvent " + hour);
+  }
+
+  var currentHour = moment().format("hh");
+  updateTimeBlocks(currentHour);
+
   minuteCounter();
-  setupRows(numHours);
+  // setupRows(numHours);
